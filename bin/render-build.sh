d79661752181bf8b2060ff4b70e9164d7d080559
rordev <rordev@gmail.com>
2025-01-16 19:37:08 +0530

    fix webpacker issue

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,11 @@ set -o errexit
 # Disable frozen Gemfile.lock
 
 bundle install
+yarn install
+
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed
+
+
